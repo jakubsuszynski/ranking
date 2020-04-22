1d392a8fc758b6d8e5478db09778cb7a81aada10
Jakub Suszynski <jakubesuszynski@gmail.com>
2020-04-22 21:25:41 +0200

    Update css

--- src/components/record.js
+++ src/components/record.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import "./record.css";
-import CardMedia from "@material-ui/core/CardMedia";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import FavoriteIcon from '@material-ui/icons/Favorite';
+import CardMedia from "@material-ui/core/CardMedia";
 
 class Record extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {url: this.prepareLink(this.props.record?.fields.url?.stringValue)}
+    }
 
     vote = (id) => {
         if (this.props.disabled) {
@@ -48,26 +52,37 @@ class Record extends React.Component {
         document.cookie = "voted=true" + ";" + expires + ";path=/";
     };
 
+    prepareLink = (url) => {
+        if (url && !url.includes("http")) {
+            return "http://" + url;
+        }
+        return url;
+    };
+
     render = () => {
+        const isMobile = window.innerWidth <= 600;
         return (
             <Card className={"page-card"}>
-                <CardMedia
-                    className={"card-image"}
-                    image={this.props.record?.fields.imgUrl?.stringValue}
-                    title={this.props.record?.fields.title?.stringValue}
-                />
-                <CardContent className={"card-content"}>
-                    <div>
-                        <Typography
-                            component="h5"
-                            variant="h5">
-                            {this.props.index + ". " + this.props.record?.fields.title?.stringValue}
-                        </Typography>
-                        <Typography
-                            variant="subtitle1"
-                            color="textSecondary">
-                            {this.props.record?.fields.url?.stringValue}
-                        </Typography>
+                <div>
+                    <CardMedia
+                        className={"card-image"}
+                        image={this.props.record?.fields.imgUrl?.stringValue}
+                        title={this.props.record?.fields.title?.stringValue}
+                    />
+                </div>
+                <CardContent className={"card-content-wrapper"}>
+                    <div className={"card-content"}>
+                        <a href={this.state.url} target="_blank">
+                            <Typography
+                                variant={isMobile ? "h6" : "h5"}>
+                                {this.props.index + ". " + this.props.record?.fields.title?.stringValue}
+                            </Typography>
+                            <Typography
+                                variant={isMobile ? "subtitle2" : "subtitle1"}
+                                color="textSecondary">
+                                {this.props.record?.fields.url?.stringValue}
+                            </Typography>
+                        </a>
                     </div>
                     <div className={"card-votes"}>
                         <div>{this.props.record?.fields.votes?.integerValue}</div>
@@ -76,7 +91,7 @@ class Record extends React.Component {
                             color={"secondary"}
                             onClick={() => this.vote(this.props.record?.name)}
                             disabled={this.props.disabled}>
-                            <FavoriteIcon fontSize={"large"} disabled={true}/>
+                            <FavoriteIcon fontSize={isMobile ? "medium" : "large"} disabled={true}/>
                         </IconButton>
                     </div>
                 </CardContent>
